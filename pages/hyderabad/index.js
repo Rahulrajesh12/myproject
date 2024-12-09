ff3d4f951c81131358a84d71adb070fdaf29b2ef
RahulRajesh <rahulrajesheerla@gmail.com>
2024-12-09 15:14:57 +0530

    added google tag

--- pages/hyderabad/index.js
+++ pages/hyderabad/index.js
@@ -17,10 +17,10 @@ import PriceList from '../components/PriceList/PriceList';
 import Head from 'next/head';
 
 export default function Place({ cars, canonicalUrl }) {
-    return ( 
+    return (
         <div>
             <Layout locname={'hyderabad'} phoneno={"9666-677-405"}>
-            <Head>
+                <Head>
                     <title> No Deposit & Unlimited km - Self-Drive Car Rentals In Hyderabad</title>
                     <meta name="description" content="Self-drive cars start at 62/hr, We offer Long Drive Cars for the best prices with unlimited km , Book clDzire @ ₹83/hr, Baleno @ ₹91/hr, Ertiga @ ₹124/hr, Swift @ ₹83/hr, Thar @ ₹208/hr." />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -40,9 +40,44 @@ export default function Place({ cars, canonicalUrl }) {
                     `,
                         }}
                     ></script>
+
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=G-8RGJTJSJCW">
+                    </script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-8RGJTJSJCW');
+                    `,
+                        }}
+                    ></script>
+
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KBCJDV6F');`,
+                        }}
+                    />
+
+
                     <link rel="canonical" href={canonicalUrl} />
                 </Head>
                 <div className="pt-32 lg:pt-0">
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-KBCJDV6F"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
                     <DynImageChange locname={'hyderabad'} />
                     <div>
                         <DynNearByApi city={'hyderabad'} />
